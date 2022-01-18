28b38531a76bb2852a1207f4a22e17db72bc1234
NikitaUrazov <urazovnikita@gmail.com>
2022-01-18 19:03:54 +0300

    Домашняя работа 5

---
+++ Homework-5/Task1-2/Task1-2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+//Написать программу, которая при старте дописывает текущее время в файл «startup.txt».
+using System;
+using System.IO;
+
+namespace Task1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "startup.txt";
+            string input;
+
+            Console.Write("Введите строку: ");
+            input = Console.ReadLine();
+
+            if (input == null)
+            {
+                Console.WriteLine("Ошибка. Пустая строка.");
+                return;
+            }
+
+            File.WriteAllText(fileName, input);
+
+            string date = Convert.ToString(DateTime.Now);
+
+            File.AppendAllText(fileName, Environment.NewLine);
+            File.AppendAllText(fileName, date.Split(' ')[0]); // Число
+            File.AppendAllText(fileName, Environment.NewLine);
+            File.AppendAllText(fileName, date.Split(' ')[1]); // Время
+
+        }
+    }
+}
---
+++ Homework-5/Task3/Task3/Program.cs
@@ -0,0 +1,41 @@
+﻿//Ввести с клавиатуры произвольный набор чисел (0...255) и записать их в бинарный файл.
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.Write("Введите произвольные числа от 0 до 255: ");
+            input = Console.ReadLine();
+
+            string[] strNumbers = input.Split(' ');
+            int[] intNumbers = new int[strNumbers.Length];
+
+            for (int i = 0; i < strNumbers.Length; i++)
+            {
+                if (!int.TryParse(strNumbers[i], out intNumbers[i]) || intNumbers[i] < 0 || intNumbers[i] > 255)
+                {
+                    Console.WriteLine("Неккоректные входные данные.");
+                    return;
+                }
+            }
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream fileStream = new FileStream("numbers.bin", FileMode.OpenOrCreate);
+            formatter.Serialize(fileStream, intNumbers);
+
+            // Чтение из bin файла
+            fileStream.Position = 0;
+            int[] newIntArray = (int[])formatter.Deserialize(fileStream);
+
+            for (int i = 0; i < newIntArray.Length; i++)
+                Console.WriteLine(newIntArray[i]);
+        }
+    }
+}
